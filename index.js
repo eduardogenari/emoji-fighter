ea22d40f52c21d686d968652a58362909b211e1e
Eduardo Genari <67627261+eduardogenari@users.noreply.github.com>
2022-08-16 13:09:49 +0200

    fix1

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    let emoji1 = randomEmoji()
-    let emoji2 = randomEmoji()
-    stageEl.textContent = emoji1 + " vs " + emoji2
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
-
-function randomEmoji() {
-    return fighters[Math.floor(Math.random()*17)]
-}
