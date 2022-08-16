5a7817c3e38c7af523d9076793ab6ee6f1f4c502
Eduardo Genari <67627261+eduardogenari@users.noreply.github.com>
2022-08-16 13:14:29 +0200

    fix3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    let randomIndexOne = Math.floor( Math.random() * fighters.length )
-    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    let randomIndexOne = Math.floor( Math.random() * (fighters.length - 1))
+    let randomIndexTwo = Math.floor( Math.random() * (fighters.length - 1))
     stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
